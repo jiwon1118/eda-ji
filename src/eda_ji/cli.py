5daa596d2b7f44691722fa144ffc09331eaf6158
jiwon1118 <b23386585@gmail.com>
2025-01-22 17:51:10 +0900

    중간저장

--- src/eda_ji/cli.py
+++ src/eda_ji/cli.py
@@ -2,6 +2,7 @@ from president_speech.db.parquet_interpreter import get_parquet_full_path
 import pandas as pd
 import typer
 
+
 def group_by_count(keyword: str, ascen: bool=False, n: int=12) -> pd.DataFrame:
     data_path = get_parquet_full_path() # 데이터 경로 가져오기 
     df = pd.read_parquet(data_path)  # 데이터 프레임으로 로드
@@ -9,11 +10,23 @@ def group_by_count(keyword: str, ascen: bool=False, n: int=12) -> pd.DataFrame:
     gdf = fdf.groupby("president").size().reset_index(name="count")  # 대통령별 그룹화 하고 개수 세기 
     sdf = gdf.sort_values(by='count', ascending=ascen).reset_index(drop=True)  # 정렬
     rdf = sdf.head(n)
-    print(rdf)
     return rdf
+
+def print_group_by_count(keyword: str, ascen: bool=False, n: int=12):
+    df = group_by_count(keyword, ascen, n)
+    print(df.to_string(index=False))
 
 # 추가 요청사항
 # 검색 단어가 등장하는 ROW 에 speech_text 속에 해당 단어가 나오는 횟수를 모두 count 하여 그 총합을 대통령별로 다시 sum
+
+#def add_keyword_count(df:pd.DataFrame, keyword: str) -> pd.DataFrame:
+    #data_path = get_parquet_full_path() # 데이터 경로 가져오기
+    #df = pd.read_parquet(data_path)  # 데이터 프레임으로 로드
+    #fdf = df[df['speech_text'].str.contains(keyword, case=False)] # 키워드가 들어간 연설 필터링
+    # 키워드가 들어간 연설문 가져오기 - fdf 
+    # 연설문에서 키워드가 몇번 들어갔는지 and sum 합계
+    # 원래 rdf에 추가하기
+
 
 def entry_point():
     typer.run(group_by_count)
--- tests/test_first.py
+++ tests/test_first.py
@@ -3,23 +3,92 @@ import pandas as pd
 
 def test_search():
     row_count = 13
-    
     # When
     df = group_by_count(keyword="자유", ascen=True, n=row_count)
     # assert
     assert isinstance(df, pd.DataFrame) 
     assert len(df) < row_count
 
-def test_search2():
+def test_정렬 및 행수제한_noascen():
     row_count = 3
     is_asc = True
-    
     # When
     df = group_by_count(keyword="자유", ascen=is_asc, n=row_count)
-    
     # assert
     assert isinstance(df, pd.DataFrame)
     assert df.iloc[0]["president"] == "윤보선"
     assert len(df) == row_count
+
+def test_정렬 및 행수제한_ascen():
+    row_count = 3
+    is_asc = False
+    # When
+    df = group_by_count(keyword="자유", ascen=is_asc, n=row_count)
+    # assert
+    assert isinstance(df, pd.DataFrame)
+    assert df.iloc[0]["president"] == "박정희"
+    assert len(df) == row_count
+
+@pytest.mark.parametrize("is_asc, president", [(True,),(False,)])
+def test_정렬 및 행수제한(is_asc,president):
+    row_count = 3
+    is_asc = True
+    # When
+    df = group_by_count(keyword="자유", ascen=is_asc, n=row_count)
+    # assert
+    assert isinstance(df, pd.DataFrame)
+    assert df.iloc[0]["president"] == "윤보선"
+    assert len(df) == row_count
+
+def test_all_count():
+    # When
+    df = group_by_count("자유") 
+    presidents_speeches = {}
+    for i in range(len(df)):
+        presidents_speeches[df.iloc[i]["president"]] = df.iloc[i]["count"]
+        #assert president_row.iloc[i]["count"] == s_count
+    # assert
+    assert presidents_speeches["박정희"] == 513 
+    assert presidents_speeches["이승만"] == 438 
+    assert presidents_speeches["노태우"] == 399 
+    assert presidents_speeches["김대중"] == 305 
+    assert presidents_speeches["문재인"] == 275
+    assert presidents_speeches["김영삼"] == 274 
+    assert presidents_speeches["이명박"] == 262 
+    assert presidents_speeches["전두환"] == 242 
+    assert presidents_speeches["노무현"] == 230 
+    assert presidents_speeches["박근혜"] == 111 
+    assert presidents_speeches["최규하"] == 14 
+    assert presidents_speeches["윤보선"] == 1 
+    
+presidents_speeches = {
+    "박정희": 513,
+    "이승만": 438,
+    "노태우": 399,
+    "김대중": 305,
+    "문재인": 275,
+    "김영삼": 274,
+    "이명박": 262,
+    "전두환": 242,
+    "노무현": 230,
+    "박근혜": 111,
+    "최규하": 14,
+    "윤보선": 1
+}
+
+def test_all_count2():
+    # given
+    # global dict
+    
+    # when
+    df = group_by_count("자유")
+    
+    # assert
+    assert isinstance(df, pd.DataFrame)
+    assert len(df) == 12
+
+    for p_name, s_count in presidents_speeches.items():
+        president_row = df[df["president"] == p_name]
+        assert president_row.iloc[0]["count"] == s_count
 
 
